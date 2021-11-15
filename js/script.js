26c5f9cbda5294563dc26283f7799d863aece8c5
heidi_b <bouchard.heidi@gmail.com>
2021-11-15 15:44:15 -0500

    displays 5 random user profiles from API

--- js/script.js
+++ js/script.js
@@ -1,1 +1,35 @@
 const randomFolks = document.querySelector(".random-peeps");
+
+const getData = async function() {
+    const usersRequest = await fetch("https://randomuser.me/api?results=5");
+    const data = await usersRequest.json();
+    console.log(data);
+
+    const usersResults = data.results;
+    console.log(usersResults);
+
+    displayUsers(usersResults);
+};
+
+getData();
+
+
+const displayUsers = function (usersResults) {
+    randomFolks.innerHTML = "";
+    for (let user of usersResults) {
+        let country = user.location.country;
+        let name = `${user.name.title}. ${user.name.first} ${user.name.last}`;
+        let imageURL = user.picture.medium;
+        
+        let userDiv = document.createElement("div");
+        userDiv.innerHTML = `
+            <h3>${name}</h3>
+            <p>${country}</p>
+            <img src=${imageURL} alt="User Avatar" />
+            `;
+        randomFolks.appendChild(userDiv);
+        console.log(user);
+    }
+};
+
+
